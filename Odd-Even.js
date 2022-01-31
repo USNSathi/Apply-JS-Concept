5c06de4aec06ea55d4aee81f9330d15953e2685e
Sadia Sathi <sadia.nasrin10@northsouth.edu>
2022-01-31 22:41:37 +0600

    Odd-Even

--- Odd-Even.js
+++ Odd-Even.js
@@ -1,6 +1,45 @@
 
 // Normal Way
 
-const number = 4;
-const remainder = number % 2;
-console.log(remainder == 0)
+// const number = 4;
+// const remainder = number % 2;
+// console.log(remainder == 0);
+// console.log(number % 2 == 0);
+
+// With Function
+
+const number = 145;
+
+function isEven(number) {
+    if (number % 2 == 0) {
+        return true;
+    }
+    return false;
+}
+
+const myNumber = 1641;
+const isMyNumberEven = isEven(myNumber);
+console.log('is my number even', isMyNumberEven);
+
+const herNumber = 1892;
+const isHerNumberEven = isEven(herNumber);
+console.log('is her number even', isHerNumberEven);
+
+// Is Odd Function
+
+function isOdd(number) {
+    // if (number % 2 == 1) {
+    //     return true;
+    // }
+    // return false;
+    if (number % 2 != 0) {
+        return true;
+    }
+    return false;
+}
+
+const isMyNumberOdd = isOdd(myNumber);
+console.log('is my number odd', isMyNumberOdd);
+
+const isHerNumberOdd = isOdd(herNumber);
+console.log('is her number odd', isHerNumberOdd);
